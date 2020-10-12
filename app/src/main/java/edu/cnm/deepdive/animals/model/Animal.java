c036c9d10acd6e717931fdb16e7b4b452c2fe4de
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-10-12 14:45:57 -0600

    Default spinner works.

--- app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
+++ app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals.model;
 
+import androidx.annotation.NonNull;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -81,5 +82,11 @@ public class Animal {
 
   public void setImageUrl(String imageUrl) {
     this.imageUrl = imageUrl;
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return getName();
   }
 }
